c43b0867c51906f27df4416c056480501e3711c2
bstrutt <bstrutt96@gmail.com>
2018-11-13 10:15:30 -0600

    Adjacency List
    Set up the adjacency list as a linked list of linked lists so that we
    didn't have to keep track of how many vertices we had in the array of
    vertices.

--- src/pa2/WGraph.java
+++ src/pa2/WGraph.java
@@ -4,13 +4,15 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class WGraph {
 	int numVertex;
 	int numEdges;
-	ArrayList[] adjList;
-	
+	LinkedList<LinkedList<edge>> adjList;		//array of linked lists representing edges of each vertex
+	LinkedList<vertex> vertexList; 	//known vertices list
+	boolean unknownVertex;
 	
 	WGraph(String FName) throws FileNotFoundException{
 		File f = new File(FName);
@@ -18,12 +20,43 @@ public class WGraph {
 		
 		numVertex = s.nextInt();				// gets the number of vertices
 		numEdges = s.nextInt();					// gets the number of edges (doesn't do much yet)
-		adjList = new ArrayList[numVertex];		// creates an array of arraylists as big as the number of vertices
 		
+		adjList = new LinkedList<>();
+		for(int i = 0; i <numVertex; i++) {
+			adjList.set(i, new LinkedList<edge>());	// Populates the array with empty LinkedLists
+		}
 		
 		
 		while(s.hasNextInt()) {
-			vertex(s.nextInt(), s.nextInt());	//This will parse the txt file to get vertex coords and edges
+			unknownVertex = true;
+			
+			// Create a temporary vertex for the newest edge
+			vertex tempVertex = new vertex(s.nextInt(), s.nextInt());
+			
+			/* 		Checks to see if the vertex is previously known
+			 		Probably more efficent ways to do this, see if there is a fix
+					Maybe something native to LinkedLists I don't know about?
+					declare i outside so we can use it for referencing vertices later
+			*/
+			int i;
+			for(i = 0; i < vertexList.size(); i++) {
+				if (vertexList.get(i).xcoord == tempVertex.xcoord && vertexList.get(i).ycoord == tempVertex.ycoord) {
+					unknownVertex = false;
+					break;
+				}
+			}
+			if(unknownVertex) {
+				//This ugly fothermucker adds a vertex to the vertex list, then appends a new edge to the corresponding 
+				//edge list of that vertex. Only if we didn't previously know the vertex; 
+				vertexList.add(tempVertex);
+				adjList.get(adjList.size()).add(new edge(tempVertex, new vertex(s.nextInt(), s.nextInt()), s.nextInt()));
+			} else {
+				//This guy adds a new edge at the location of the known vertex.
+				adjList.get(i).add(new edge(tempVertex, new vertex(s.nextInt(), s.nextInt()), s.nextInt()));
+			}
+			
+			
+			
 														
 		}
 	}
